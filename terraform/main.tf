72e98146e21354a333fa8f697bfe72f2e0310923
carlostr <xdeus.v2@gmail.com>
2026-04-07 12:30:48 -0600

    feat: modificar hosts permitidos de ssh y actualizar terraform state

--- terraform/main.tf
+++ terraform/main.tf
@@ -18,7 +18,7 @@ resource "aws_security_group" "ssh" {
     from_port   = 22
     to_port     = 22
     protocol    = "tcp"
-    cidr_blocks = ["${var.my_ip}/32"]
+    cidr_blocks = ["0.0.0.0/0"]
   }
 
   ingress {
